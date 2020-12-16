bef4a21f88cc791069363e63395fef5774bc4191
iseunghan <tmdgks301@naver.com>
2020-12-16 22:58:29 +0900

    ch10. 객체지향 쿼리 언어 프로젝션 공부

---
+++ src/main/java/chpater10_객체지향쿼리언어/MemberDTO.java
@@ -0,0 +1,27 @@
+package chpater10_객체지향쿼리언어;
+
+public class MemberDTO {
+    int age;
+    String username;
+
+    public MemberDTO(int age, String username) {
+        this.age = age;
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
